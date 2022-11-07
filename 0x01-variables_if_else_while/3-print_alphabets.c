0a885a9222905146c42374cfa421c8d0ff82e48d
AdamAbdulkareem <dmabdulkareem@gmail.com>
2022-11-07 01:20:59 -0800

    Modify 3-print_alphabets.c file and create 3-print_alphabets file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - Entry point
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	int lowerChar;
+	int upperChar;
+	for (lowerChar = 'a'; lowerChar <= 'z'; lowerChar++)
+	{
+		putchar(lowerChar);
+		putchar('\n');
+	}
+	for (upperChar = 'A'; upperChar <= 'Z'; upperChar++)
+	{
+		putchar(upperChar);
+		putchar('\n');
+	}
+	return 0;
+}
