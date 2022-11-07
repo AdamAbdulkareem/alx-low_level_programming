e0f3e2ba5c133d62b287253c62b315f1198d084e
AdamAbdulkareem <dmabdulkareem@gmail.com>
2022-11-07 00:32:26 -0800

    Modify 2-print_alphabet.c file and created 2-print_alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int character;
+		for (character = 'a'; character <= 'z'; character++)
+		{
+			putchar(character);
+		}
+	return (0);
+}
