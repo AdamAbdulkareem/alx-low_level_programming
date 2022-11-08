92ccbce4a3a2c98b8d208ab53c1a48b44ec90c27
AdamAbdulkareem <dmabdulkareem@gmail.com>
2022-11-08 06:43:33 -0800

    Modify 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point of the program
+  * Return: Always 0
+  */
+int main(void)
+{
+	char lowerChar;
+		for (lowerChar = 'a'; lowerChar <= 'z'; letter++)
+		{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+		}
+
+	putchar('\n');
+	return (0);
+}
