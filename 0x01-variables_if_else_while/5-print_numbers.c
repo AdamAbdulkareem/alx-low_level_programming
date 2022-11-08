fdaac16f34f59f45cf3c7d00902d436290995a66
AdamAbdulkareem <dmabdulkareem@gmail.com>
2022-11-08 07:40:08 -0800

    Modify 5-print_numbers.c file and Create 5-print_numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Entry point of the program
+  * Return: Always 0
+  */
+int main(void)
+{
+	int digit;
+		for (digit = 0; digit <= 9; digit++)
+		{
+			printf("%d", digit);
+		}
+	return (0);
+}
