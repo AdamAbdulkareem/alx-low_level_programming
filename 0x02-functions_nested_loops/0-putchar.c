26a839338ad439032e1f0475e5df13424f2f8cdd
AdamAbdulkareem <dmabdulkareem@gmail.com>
2022-11-08 09:03:28 -0800

    Modify 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main -Entry point of the program
+  * Return: Always 0
+  */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
