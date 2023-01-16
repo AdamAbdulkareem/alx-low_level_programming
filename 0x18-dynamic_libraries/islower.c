e07f7bb213bcf0d68f39bdc37bdceaf245ae5f35
Adam Abdulkareem <dmabdulkareem@gmail.com>
2023-01-16 15:38:11 +0100

    Update islower.c

--- 0x18-dynamic_libraries/islower.c
+++ 0x18-dynamic_libraries/islower.c
@@ -1,9 +1,18 @@
 #include "main.h"
 /**
-  * _islower - A function
+  * _islower - check for lowercase character
   * @c: An input character
-  * Return: An integer
+  * Return: 1 if c is lowercase, 0 if otherwise
   */
 int _islower(int c)
 {
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+	return (lower);
 }
