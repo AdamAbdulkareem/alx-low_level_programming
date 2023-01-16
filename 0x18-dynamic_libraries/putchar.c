d965381a9f26169b834647e73b1ee44f7dc50bb9
Adam Abdulkareem <dmabdulkareem@gmail.com>
2023-01-16 15:37:04 +0100

    Update putchar.c

--- 0x18-dynamic_libraries/putchar.c
+++ 0x18-dynamic_libraries/putchar.c
@@ -1,9 +1,19 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
-  * _putchar - A function
-  * @c: An input character
-  * Return: An integer
+  * main - Check description
+  * Description: it prints the word _putchar, followed by a new line
+  * Return: Always 0
   */
-int _putchar(char c)
+int main(void)
 {
+	char word[8] = "_putchar";
+	int i;
+		for (i = 0; i < 8; i++)
+		{
+		_putchar(word[i]);
+		}
+		_putchar('\n');
+	return (0);
 }
