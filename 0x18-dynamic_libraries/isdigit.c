c472f966e97c0e77c32f78fdce50ce663f5149c5
Adam Abdulkareem <dmabdulkareem@gmail.com>
2023-01-16 15:39:54 +0100

    Update isdigit.c

--- 0x18-dynamic_libraries/isdigit.c
+++ 0x18-dynamic_libraries/isdigit.c
@@ -1,9 +1,18 @@
 #include "main.h"
 /**
-  * _isdigit - A function
-  * @c: An input character
-  * Return: An integer
+  * _isdigit - The function checks for a digit
+  * @c: C is an integer
+  * Return: Always return 1 and 0
   */
 int _isdigit(int c)
 {
+	char a;
+	int digit = 0;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a == c)
+			digit = 1;
+	}
+	return (digit);
 }
