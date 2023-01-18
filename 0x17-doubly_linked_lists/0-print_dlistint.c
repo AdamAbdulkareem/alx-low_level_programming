2b02265e1438c777a29d431226efcc5a96ebb4e4
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-18 06:50:03 -0800

    Modify 0-print_dlistint.c file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,22 +2,21 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
-  * print_dlistint - a function that prints the number of node 
-  * @dlistint_t: An input character
-  * Return: Always size_t
- */
-size_t print_dlistint(const dlistint_t *h)
+ * print_dlistint - a function that prints the number of node
+ * @h: An input character
+ * Return: Always size_t
+*/
+size_t print_dlistint (const dlistint_t *h)
 {
 	int count = 0;
 
-	if(h == NULL)
+	if (h == NULL)
 	{
 		return (count);
 	}
 	pointer = h;
-	while(pointer != NULL)
+	while (pointer != NULL)
 	{
 		count++;
 		printf("%d\n", pointer->n);
